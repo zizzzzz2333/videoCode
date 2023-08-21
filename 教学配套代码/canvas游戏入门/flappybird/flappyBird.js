198d6897732c3cc17eb1b5aa7f2125bfd5cf856a
zizzzzz2333 <2226549571@qq.com>
2023-08-21 19:56:18 +0800

    update

--- 教学配套代码/canvas游戏入门/flappybird/flappyBird.js
+++ 教学配套代码/canvas游戏入门/flappybird/flappyBird.js
@@ -22,22 +22,22 @@ const entry = async () => {
     }
 
     const imgsList = [
-        {name: '0', src: '../imgs/0.png'},
-        {name: '1', src: '../imgs/1.png'},
-        {name: '2', src: '../imgs/2.png'},
-        {name: '3', src: '../imgs/3.png'},
-        {name: '4', src: '../imgs/4.png'},
-        {name: '5', src: '../imgs/5.png'},
-        {name: '6', src: '../imgs/6.png'},
-        {name: '7', src: '../imgs/7.png'},
-        {name: '8', src: '../imgs/8.png'},
-        {name: '9', src: '../imgs/9.png'},
-        {name: 'bg', src: '../imgs/bg.png'},
-        {name: 'bird', src: '../imgs/b-01.png'},
-        {name: 'ground', src: '../imgs/ground.png'},
-        {name: 'pipe', src: '../imgs/pipe.png'},
-        {name: 'message', src: '../imgs/message.png'},
-        {name: 'gameover', src: '../imgs/gameover.png'},
+        {name: '0', src: './imgs/0.png'},
+        {name: '1', src: './imgs/1.png'},
+        {name: '2', src: './imgs/2.png'},
+        {name: '3', src: './imgs/3.png'},
+        {name: '4', src: './imgs/4.png'},
+        {name: '5', src: './imgs/5.png'},
+        {name: '6', src: './imgs/6.png'},
+        {name: '7', src: './imgs/7.png'},
+        {name: '8', src: './imgs/8.png'},
+        {name: '9', src: './imgs/9.png'},
+        {name: 'bg', src: './imgs/bg.png'},
+        {name: 'bird', src: './imgs/b-01.png'},
+        {name: 'ground', src: './imgs/ground.png'},
+        {name: 'pipe', src: './imgs/pipe.png'},
+        {name: 'message', src: './imgs/message.png'},
+        {name: 'gameover', src: './imgs/gameover.png'},
     ]
     const imgs = await loadImages(imgsList.map(img => img.src))
     const imgNames = imgsList.map(img => img.name)
